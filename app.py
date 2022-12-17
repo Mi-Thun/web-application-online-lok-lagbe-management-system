f7f97c8e3b54885effa38374ca79e8eddec14f70
Mohsenul Kabir Mithun <mk_mithun@yahoo.com>
2022-12-17 14:58:14 +0600

    First Commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/signup/')
+def signup():
+    return render_template("signup.html")
+
+
+@app.route('/contact/')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/blog/')
+def blog():
+    return render_template("blog.html")
+
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/user/')
+def user():
+    return render_template("user.html")
+
+
+@app.route('/service/')
+def service():
+    return render_template("service.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
