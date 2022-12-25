325b7047cf676450926d5e45415f713e28bf9937
Mohsenul Kabir Mithun <mk_mithun@yahoo.com>
2022-12-26 01:30:55 +0600

    Login user selection Done

--- app.py
+++ app.py
@@ -38,12 +38,16 @@ def login():
 def signup():
     if request.method == "POST":
         form_data = request.form
+        print(form_data)
         name = form_data["name"]
         email = form_data["email"]
         pass1 = form_data["pass"]
         pass2 = form_data["re_pass"]
         agree = form_data['agree-term']
-        worker_type = form_data['worker_type']
+        try:
+            worker_type = str(form_data['agree-term1'])
+        except:
+            worker_type = 'no'
         user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': worker_type})
         message = "Register Successfully"
         return render_template("index.html ", **locals())
@@ -54,60 +58,8 @@ def signup():
 def userprofile():
     isPost = False
     visibility1 = 'hidden'
-    result = user_info.find_one({"email": 'mk@gmail.com'})
-    info = worker_info.find_one({"email": 'mk@gmail.com'})
-
-    if request.method == "POST":
-        isPost = True
-
-    if result['workertype'] == 'on':
-        visibility1 = 'visible'
-        if isPost:
-            form_data = request.form
-            print(form_data)
-            name = form_data["Name"]
-            phone = form_data["Phone Number"]
-            age = form_data["Age"]
-            sex = form_data["Sex"]
-            email = form_data['Email ID']
-            nid = form_data['NID']
-            address = form_data["Address"]
-            postcode = form_data['Postcode']
-            area = form_data['Area']
-            city = form_data["City"]
-            parea = form_data['Preferred area']
-            experience = form_data['Experience']
-            additional = form_data["Additional Details"]
-            sat = form_data['Saturday']
-            sun = form_data['Sunday']
-            mon = form_data['Monday']
-            tue = form_data["Tuesday"]
-            wed = form_data['Wednesday']
-            thr = form_data['Thursday']
-            ffri = form_data["Friday"]
-            type = form_data['work_type']
-            charge = form_data['Charge per hour']
-            pass1 = result['pass']
-            user_info.delete_one({"email": 'mk@gmail.com'})
-            user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': result['worker_type']})
-            dict = {'name': name, 'phone': phone, 'age': age, 'sex': sex,
-                                  'email': email, 'nid': nid, 'address': address, 'postcode': postcode,
-                                  'area': area, 'city': city, 'parea': parea, 'experience': experience,
-                                  'additional': additional, 'sat': sat, 'sun': sun, 'mon': mon,
-                                  'tue': tue, 'wed': wed, 'thr': thr, 'ffri': ffri, 'type': type,
-                                  'charge': charge, 'pass1': pass1}
-            print(dict)
-            worker_info.insert_one(dict)
-            return render_template("index.html", **locals())
-    return render_template("userprofile.html", **locals())
-
-
-@app.route('/contact/')
-def contact():
-    isPost = False
-    visibility1 = 'hidden'
-    result = user_info.find_one({"email": 'mk@gmail.com'})
-    info = worker_info.find_one({"email": 'mk@gmail.com'})
+    result = user_info.find_one({"email": 'mi@gmail.com'})
+    info = worker_info.find_one({"email": 'mi@gmail.com'})
 
     if request.method == "POST":
         isPost = True
@@ -151,6 +103,39 @@ def contact():
             print(dict)
             worker_info.insert_one(dict)
             return render_template("index.html", **locals())
+    return render_template("userprofile.html", **locals())
+
+
+@app.route('/contact/')
+def contact():
+    # isPost = False
+    # if request.method == "POST":
+    #     isPost = True
+    #
+    # if isPost:
+    #     form_data = request.form
+    #     print(form_data)
+    #     name = form_data["Name"]
+    #         phone = form_data["Phone Number"]
+    #         age = form_data["Age"]
+    #         sex = form_data["Sex"]
+    #         email = form_data['Email ID']
+    #         nid = form_data['NID']
+    #         address = form_data["Address"]
+    #         postcode = form_data['Postcode']
+    #         area = form_data['Area']
+    #
+    #         user_info.delete_one({"email": 'mk@gmail.com'})
+    #         user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': result['worker_type']})
+    #         dict = {'name': name, 'phone': phone, 'age': age, 'sex': sex,
+    #                 'email': email, 'nid': nid, 'address': address, 'postcode': postcode,
+    #                 'area': area, 'city': city, 'parea': parea, 'experience': experience,
+    #                 'additional': additional, 'sat': sat, 'sun': sun, 'mon': mon,
+    #                 'tue': tue, 'wed': wed, 'thr': thr, 'ffri': ffri, 'type': type,
+    #                 'charge': charge, 'pass1': pass1}
+    #         print(dict)
+    #         worker_info.insert_one(dict)
+    #         return render_template("index.html", **locals())
     return render_template("contact.html")
 
 
