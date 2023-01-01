f04a60ad729db71bb7c7d5587b60d66801505bcf
Mohsenul Kabir Mithun <mk_mithun@yahoo.com>
2023-01-01 21:47:24 +0600

    session, user profile, login condition, signup condition done

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import *
 from flask import Flask, render_template, redirect, request, session
 from flask_session import Session
 import pymongo
+from flask_mail import *
 
 app = Flask(__name__)
+
+app.secret_key = 'super secret key'
 
 myDB = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myDB["Lok_Lagbe"]
@@ -12,6 +15,7 @@ worker_info = mydb["worker_info"]
 deal_info = mydb["deal_info"]
 feedback = mydb["feedback"]
 contact_info = mydb["contact_info"]
+client_info = mydb["client_info"]
 
 
 @app.route("/")
@@ -21,51 +25,80 @@ def index():
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
+    error = ""
+    try:
+        if session["logged_in"]:
+            return render_template("index.html ", **locals())
+    except:
+        print('')
     if request.method == "POST":
         form_data = request.form
         useremail = form_data["your_email"]
         password = form_data["your_pass"]
-        error = ""
-        result = user_info.find_one({"email": useremail, "pass1": password})
+        result = user_info.find_one({"email": useremail, "pass": password})
         if result is None:
             error = "Enter valid information"
         else:
             error = "Login Successful."
-        return render_template("index.html ", **locals())
+        if error == "Login Successful.":
+            session["logged_in"] = True
+            session["email"] = useremail
+            session["name"] = result['name']
+            return render_template("index.html ", **locals())
     return render_template("login.html", **locals())
 
 
 @app.route('/signup/', methods=["GET", "POST"])
 def signup():
+    isPost = False
+    message = ''
+    agree = 'no'
     if request.method == "POST":
+        isPost = True
         form_data = request.form
-        print(form_data)
         name = form_data["name"]
         email = form_data["email"]
         pass1 = form_data["pass"]
         pass2 = form_data["re_pass"]
-        agree = form_data['agree-term']
         try:
             worker_type = str(form_data['agree-term1'])
         except:
             worker_type = 'no'
-        user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': worker_type})
-        message = "Register Successfully"
-        return render_template("index.html ", **locals())
-    return render_template("signup.html")
+        try:
+            agree = form_data['agree-term']
+        except:
+            agree = 'no'
+        if agree == 'no':
+            message = 'please agree our terms and condition'
+            print(message)
+        else:
+            check_user = user_info.find_one({"email": email})
+            if pass1 == pass2:
+                if check_user is not None:
+                    message = 'This email address already register'
+                    print(message)
+                else:
+                    user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': worker_type})
+                    message = "Successfully complete registration"
+                    print(message)
+            else:
+                message = 'Enter same password'
+                print(message)
+            if message == "Successfully complete registration":
+                return render_template("index.html ", **locals())
+    return render_template("signup.html", **locals())
 
 
 @app.route('/userprofile/', methods=["GET", "POST"])
 def userprofile():
     isPost = False
     visibility1 = 'hidden'
-    result = user_info.find_one({"email": 'mk@gmail.com'})
-    info = worker_info.find_one({"email": 'mk@gmail.com'})
-
+    result = user_info.find_one({"email": session["email"]})
     if request.method == "POST":
         isPost = True
 
     if result['workertype'] == 'on':
+        info = worker_info.find_one({"email": session["email"]})
         visibility1 = 'visible'
         if isPost:
             form_data = request.form
@@ -93,16 +126,43 @@ def userprofile():
             type = form_data['work_type']
             charge = form_data['Charge per hour']
             pass1 = result['pass']
-            user_info.delete_one({"email": 'mk@gmail.com'})
-            user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': result['worker_type']})
+            user_info.delete_one({"email": session["email"]})
+            user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': result['workertype']})
             dict = {'name': name, 'phone': phone, 'age': age, 'sex': sex,
                     'email': email, 'nid': nid, 'address': address, 'postcode': postcode,
                     'area': area, 'city': city, 'parea': parea, 'experience': experience,
                     'additional': additional, 'sat': sat, 'sun': sun, 'mon': mon,
                     'tue': tue, 'wed': wed, 'thr': thr, 'ffri': ffri, 'type': type,
                     'charge': charge, 'pass1': pass1}
-            print(dict)
             worker_info.insert_one(dict)
+            session["email"] = email
+            session["name"] = name
+            return render_template("index.html", **locals())
+
+    if result['workertype'] == 'no':
+        info = client_info.find_one({"email": session["email"]})
+        if isPost:
+            form_data = request.form
+            print(form_data)
+            name = form_data["Name"]
+            phone = form_data["Phone Number"]
+            age = form_data["Age"]
+            sex = form_data["Sex"]
+            email = form_data['Email ID']
+            nid = form_data['NID']
+            address = form_data["Address"]
+            postcode = form_data['Postcode']
+            area = form_data['Area']
+            city = form_data["City"]
+            pass1 = result['pass']
+            user_info.delete_one({"email": session["email"]})
+            user_info.insert_one({'name': name, 'email': email, 'pass': pass1, 'workertype': result['workertype']})
+            dict = {'name': name, 'phone': phone, 'age': age, 'sex': sex,
+                    'email': email, 'nid': nid, 'address': address, 'postcode': postcode,
+                    'area': area, 'city': city}
+            client_info.insert_one(dict)
+            session["email"] = email
+            session["name"] = name
             return render_template("index.html", **locals())
     return render_template("userprofile.html", **locals())
 
@@ -119,7 +179,6 @@ def contact():
         email = form_data["email"]
         subject = form_data["subject"]
         message = form_data["message"]
-
         dict = {'name': name, 'email': email, 'subject': subject, 'message': message}
         contact_info.insert_one(dict)
     return render_template("contact.html")
@@ -130,18 +189,35 @@ def blog():
     return render_template("blog.html")
 
 
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return render_template("index.html", **locals())
+
 @app.route('/about/')
 def about():
     return render_template("about.html")
 
 
-@app.route('/service/<string:n>/')
+@app.route('/service/<string:n>/', methods=["GET", "POST"])
 def service(n):
     n = str(n)
-    print(n)
     list = []
+    listS = []
+    isPost = False
+    if request.method == 'POST':
+        isPost = True
+        f = request.form
+        s = f['search']
+        # s = s.split(" ")
+        for form_data in mydb.worker_info.find({'type': n, 'area': f['search']}):
+            listS.append(form_data)
     for form_data in mydb.worker_info.find({'type': n}):
         list.append(form_data)
+    if len(listS)==0:
+        message = 'No data found'
+    else:
+        message = 'Your search result:'
     return render_template("service.html", **locals())
 
 
