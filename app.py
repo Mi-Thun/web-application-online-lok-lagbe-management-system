ad66fc5ff9b66632f936ea0b77d795c61398f171
Mohsenul Kabir Mithun <mk_mithun@yahoo.com>
2023-01-16 13:58:41 +0600

    Organization back end, blog login require done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from random import randint
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect
 import pymongo
 from bson.objectid import ObjectId
 from flask_mail import *
@@ -26,6 +26,7 @@ mydb = myDB["Lok_Lagbe"]
 user_info = mydb["user_info"]
 worker_info = mydb["worker_info"]
 deal_info = mydb["deal_info"]
+organization_deal_info = mydb["organization_deal_info"]
 feedback = mydb["feedback"]
 contact_info = mydb["contact_info"]
 client_info = mydb["client_info"]
@@ -51,7 +52,7 @@ def index():
             else:
                 haveNoti = False
     except:
-        c =''
+        c = ''
     if request.args.get('id') is not None:
         idf = request.args.get('id')
         a = deal_info.find_one({'_id': ObjectId(idf)})
@@ -77,25 +78,28 @@ def index():
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
+    print(session)
     error = ""
     try:
         if session["logged_in"]:
             return render_template("index.html ", **locals())
     except:
-        if request.method == "POST":
-            form_data = request.form
-            useremail = form_data["your_email"]
-            password = form_data["your_pass"]
-            result = user_info.find_one({"email": useremail, "pass": password})
-            if result is None:
-                error = "Enter valid information"
-            else:
-                error = "Login Successful."
-            if error == "Login Successful.":
-                session["logged_in"] = True
-                session["email"] = useremail
-                session["name"] = result['name']
-                return render_template("index.html ", **locals())
+        c = ''
+    if request.method == "POST":
+        form_data = request.form
+        print(form_data)
+        useremail = form_data["your_email"]
+        password = form_data["your_pass"]
+        result = user_info.find_one({"email": useremail, "pass": password})
+        if result is None:
+            error = "Enter valid information"
+        else:
+            error = "Login Successful."
+        if error == "Login Successful.":
+            session["logged_in"] = True
+            session["email"] = useremail
+            session["name"] = result['name']
+            return render_template("index.html ", **locals())
     return render_template("login.html", **locals())
 
 
@@ -237,52 +241,70 @@ def contact():
 
 @app.route('/organization/', methods=["GET", "POST"])
 def organization():
+    isPost = False
+    if request.method == "POST":
+        isPost = True
+    if isPost:
+        form_data = request.form
+        d = dict(form_data)
+        d['submitterEmail'] = session['email']
+        organization_deal_info.insert_one(d)
+        return render_template("index.html", **locals())
     return render_template("organization.html")
 
 
 @app.route('/blog/', methods=["GET", "POST"])
 def blog():
-    edit = False
-    if request.args.get('id') is not None:
-        if request.args.get('typ') == 'dlt':
-            idf = request.args.get('id')
-            Blog_Info.delete_one({'_id': ObjectId(idf)})
-        if request.args.get('typ') == 'edit':
-            edit = True
-            idf = request.args.get('id')
-            infoB = Blog_Info.find_one({'_id': ObjectId(idf)})
-            pre_tittle = infoB['tittle']
-            pre_content = infoB['content']
-            pre_id = infoB['_id']
-            pre_photo = infoB['image']
-    else:
-        f = "Don't have any id"
-    list = []
-    havePost = False
-    isPost = False
-    for data in Blog_Info.find():
-        a = user_info.find_one({'email': data['email']})
-        data['name'] = a['name']
-        list.append(data)
-        print(data)
-        havePost = True
-    print(list)
-    result = ''
-    isPost = False
-    if request.method == "POST":
-        isPost = True
-        email = session['email']
-        tittle = request.form['tittle']
-        content = request.form['content']
-        image = request.form['image']
-        try:
-            Blog_Info.delete_one({'_id': ObjectId(request.form['ide'])})
-        except:
-            print('done')
-        Blog_Info.insert_one({'tittle': tittle, 'content': content, 'image': image, 'email': email})
-        result = "Insert Successfully"
-        print({'tittle': tittle, 'content': content, 'image': image})
-        return render_template("blog.html", **locals())
+    try:
+        if session["logged_in"]:
+            session["logged_in"] = True
+    except:
+        session["logged_in"] = False
+    print(session["logged_in"])
+    if session["logged_in"] == False:
+        return render_template("login.html", **locals())
+    if session["logged_in"] == True:
+        edit = False
+        if request.args.get('id') is not None:
+            if request.args.get('typ') == 'dlt':
+                idf = request.args.get('id')
+                Blog_Info.delete_one({'_id': ObjectId(idf)})
+            if request.args.get('typ') == 'edit':
+                edit = True
+                idf = request.args.get('id')
+                infoB = Blog_Info.find_one({'_id': ObjectId(idf)})
+                pre_tittle = infoB['tittle']
+                pre_content = infoB['content']
+                pre_id = infoB['_id']
+                pre_photo = infoB['image']
+        else:
+            f = "Don't have any id"
+        list = []
+        havePost = False
+        isPost = False
+        for data in Blog_Info.find():
+            a = user_info.find_one({'email': data['email']})
+            data['name'] = a['name']
+            list.append(data)
+            print(data)
+            havePost = True
+        print(list)
+        result = ''
+        isPost = False
+        if request.method == "POST":
+            isPost = True
+            email = session['email']
+            tittle = request.form['tittle']
+            content = request.form['content']
+            image = request.form['image']
+            try:
+                Blog_Info.delete_one({'_id': ObjectId(request.form['ide'])})
+            except:
+                print('done')
+            Blog_Info.insert_one({'tittle': tittle, 'content': content, 'image': image, 'email': email})
+            result = "Insert Successfully"
+            print({'tittle': tittle, 'content': content, 'image': image})
+            return render_template("blog.html", **locals())
     return render_template("blog.html", **locals())
 
 
@@ -294,7 +316,10 @@ def logout():
 
 @app.route('/about/')
 def about():
-    return render_template("about.html")
+    NumberOfworker = len(list(worker_info.find()))
+    NumberOfClient = len(list(client_info.find()))
+    NumberOfdeal = len(list(deal_info.find()))
+    return render_template("about.html", **locals())
 
 
 @app.route('/forgetPassword/', methods=["GET", "POST"])
@@ -314,15 +339,17 @@ def forgetPassword():
                               recipients=[email])
                 msg.body = "http://127.0.0.1:5000/changePassword/" + str(user['_id']) + str(otp)
                 mail.send(msg)
+                return message
                 time.sleep(300)
                 pass_recover_info.delete_one({'email': email, 'otp': otp})
+            else:
+                message = 'account with this email address not found'
     return render_template("forgetPassword.html", **locals())
 
 
 @app.route('/changePassword/<string:n>', methods=["GET", "POST"])
 def changePassword(n):
     a = pass_recover_info.find_one({'otp': int(n[24:])})
-    print(a)
     find = user_info.find_one({'_id': ObjectId(n[:24])})
     if request.method == 'POST':
         pass1 = request.form['pass1']
@@ -333,7 +360,12 @@ def changePassword(n):
                 user_info.delete_one({'email': find['email']})
                 user_info.insert_one(dict(find))
                 pass_recover_info.delete_one({'email': find['email']})
+                message = 'Password changes done'
                 return render_template("login.html", **locals())
+            else:
+                message = 'Enter same password...'
+        else:
+            message = 'Try again'
     return render_template("changePassword.html", **locals())
 
 
@@ -403,7 +435,7 @@ def client():
                 'name': name, 'phonenumber': phonenumber, 'email': email, 'seen': 'no'}
         deal_info.insert_one(dict)
         msg = Message('New offer arrived', sender='mohsenulkabirmi8486@gmail.com', recipients=[worker_email])
-        msg.body = "You have a new offer by '"+ name + "'.Work Details, start_date:"+ start_date+ '; end_date:'+ end_date+ '; Start_Time:'+ Start_Time+ '; end_time:'+ end_time+'; address:'+address+'; Work_Description:'+ Work_Description+'; phonenumber:'+phonenumber+'; email:'+email
+        msg.body = "You have a new offer by '" + name + "'.Work Details, start_date:" + start_date + '; end_date:' + end_date + '; Start_Time:' + Start_Time + '; end_time:' + end_time + '; address:' + address + '; Work_Description:' + Work_Description + '; phonenumber:' + phonenumber + '; email:' + email
         mail.send(msg)
         return render_template("index.html", **locals())
     return render_template("client.html", **locals())
